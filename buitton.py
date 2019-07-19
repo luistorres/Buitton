0a9794dc1a7f148520091ec294bc46c55ca9eba0
Luís Torres <luistorres1792@gmail.com>
2019-07-19 12:29:26 +0100

    revert loop changes

--- buitton.py
+++ buitton.py
@@ -20,7 +20,7 @@ def do_play(x):
     while pygame.mixer.music.get_busy():
         pass
 
-    pygame.mixer.music.play(1)
+    pygame.mixer.music.play(2)
 
 def stop():
     return lambda: do_stop()
